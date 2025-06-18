935a38a03f8eedbbc9f225e99891267455640ac4
Vegard R. Solum <vegard.solum@4subsea.com>
2025-06-18 09:47:42 +0200

    blueprint

--- src/mlserve_api/__init__.py
+++ src/mlserve_api/__init__.py
@@ -1,61 +1,16 @@
-from flask import Flask, jsonify, request, abort
-import numpy as np
+from flask import Flask, jsonify
 
-from ._db import base_url, INDEX, MODELS
+from ._db import INDEX
+from ._models import bp as models_bp
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
+    app.register_blueprint(models_bp)
 
 
     @app.route("/")
     def index():
         return jsonify(INDEX)
-    
-    @app.get("/models")
-    def get_models():
-        return jsonify(MODELS)
-
-
-    @app.post("/models")
-    def post_models():
-
-        data = request.get_json()
-        if "name" not in data:
-            abort(400, description="Name is required")
-
-        new_id = max(model["id"] for model in MODELS) + 1 if MODELS else 1
-        new_model = {
-            "id": new_id,
-            "name": data["name"],
-            "url": f"{base_url}/models/{new_id}",
-            "prediction_url": f"{base_url}/models/{new_id}/prediction",
-        }
-        MODELS.append(new_model)
-        return jsonify(new_model), 201
-
-
-    @app.get("/models/<int:model_id>")
-    def get_model(model_id):
-        """Retrieve the details for a specific model."""
-        model = next((m for m in MODELS if m["id"] == int(model_id)), None)
-        return jsonify(model)
-
-
-    @app.post("/models/<int:model_id>/prediction")
-    def prediction(model_id):
-        """Predict operability using a specific ML model."""
-        model = next((m for m in MODELS if m["id"] == model_id), None)
-        if not model:
-            abort(404, description="Model not found")
-
-        pred = int(np.random.default_rng().choice([0, 1], p=[0.9, 0.1]))
-
-        response_dict = {
-            "model_id": model_id,
-            "prediction": pred,
-        }
-
-        return jsonify(response_dict), 200
 
     return app
---
+++ src/mlserve_api/_models.py
@@ -0,0 +1,54 @@
+import numpy as np
+from flask import Blueprint, jsonify, request, abort
+
+from ._db import base_url, MODELS
+
+
+bp = Blueprint('models', __name__, url_prefix='/models')
+
+
+@bp.get("/")
+def get_models():
+    return jsonify(MODELS)
+
+
+@bp.post("/")
+def post_models():
+
+    data = request.get_json()
+    if "name" not in data:
+        abort(400, description="Name is required")
+
+    new_id = max(model["id"] for model in MODELS) + 1 if MODELS else 1
+    new_model = {
+        "id": new_id,
+        "name": data["name"],
+        "url": f"{base_url}/models/{new_id}",
+        "prediction_url": f"{base_url}/models/{new_id}/prediction",
+    }
+    MODELS.append(new_model)
+    return jsonify(new_model), 201
+
+
+@bp.get("/<int:model_id>")
+def get_model(model_id):
+    """Retrieve the details for a specific model."""
+    model = next((m for m in MODELS if m["id"] == int(model_id)), None)
+    return jsonify(model)
+
+
+@bp.post("/<int:model_id>/prediction")
+def prediction(model_id):
+    """Predict operability using a specific ML model."""
+    model = next((m for m in MODELS if m["id"] == model_id), None)
+    if not model:
+        abort(404, description="Model not found")
+
+    pred = int(np.random.default_rng().choice([0, 1], p=[0.9, 0.1]))
+
+    response_dict = {
+        "model_id": model_id,
+        "prediction": pred,
+    }
+
+    return jsonify(response_dict), 200
